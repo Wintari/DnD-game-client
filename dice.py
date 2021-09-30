0be6ea5c49ad75332e414b92f1bf4175266c1d93
Wintari <dimonx2000.ru@mail.ru>
2021-09-30 06:41:32 +0300

    Dice module complete

---
+++ dice.py
@@ -0,0 +1,35 @@
+import random
+import enum
+
+class ThrowTypes(enum.Enum):
+    summ = 0,
+    advantage = 1,
+    hindrance = 2,
+    withoutExtreme = 3
+
+
+def throw(edges):
+    return random.randint(1, edges)
+
+def throwMany(count, edges, type):
+    throws = [throw(edges) for i in range(0, count)]
+
+    if(type is ThrowTypes.summ):
+        return sum(throws)
+    elif(type is ThrowTypes.advantage):
+        return max(throws)
+    elif(type is ThrowTypes.hindrance):
+        return min(throws)
+    elif(type is ThrowTypes.withoutExtreme):
+        throws.remove(min(throws))
+        throws.remove(max(throws))
+
+        return sum(throws)
+
+
+
+if __name__ == "__main__":
+    print(throwMany(6, 6, ThrowTypes.summ))
+    print(throwMany(2, 6, ThrowTypes.advantage))
+    print(throwMany(2, 6, ThrowTypes.hindrance))
+    print(throwMany(6, 6, ThrowTypes.withoutExtreme))
