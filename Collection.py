0f4539fc0c82ee0b1bb2dde70555263f1e8a220d
MatrosovDenis <51385717+MatrosovDenis@users.noreply.github.com>
2021-10-01 00:06:20 +0300

    Rename classes

--- Collection.py
+++ Collection.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Form(object):
+class CollectionForm(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(1198, 850)
--- Trade.py
+++ Trade.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Form(object):
+class TradeForm(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(1007, 651)
