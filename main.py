fad44ad00561cd894c345f0a106aaf399205c497
Daniil <42893347+DanMsl@users.noreply.github.com>
2021-10-01 01:27:05 +0300

    add main

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyQt5 import QtWidgets
+import sys
+from form import Ui_PlayingField
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    ui = Ui_PlayingField()
+    window = QtWidgets.QMainWindow()
+    ui.setupUi(window)
+
+    window.show()
+    app.exec()
